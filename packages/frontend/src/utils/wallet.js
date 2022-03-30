5ba9e43d23b0996554711e31a25ca9b67827bda8
gutsyphilip <philip.c.obosi@gmail.com>
2022-03-30 04:16:10 +0400

    fix: implicit account flow to use custom hd path

--- packages/frontend/src/utils/wallet.js
+++ packages/frontend/src/utils/wallet.js
@@ -547,8 +547,6 @@ class Wallet {
 
     async getLedgerAccountIds({ path }) {
         const publicKey = await this.getLedgerPublicKey(path);
-
-        // TODO: getXXX methods shouldn't be modifying the state
         await setKeyMeta(publicKey, { type: 'ledger' });
 
         let accountIds;
